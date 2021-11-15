7314ce779b45c3750433d0a67d372187c3f06fbe
eternal17 <sarmadkhatri15@gmail.com>
2021-11-15 19:41:35 +0000

    scanning lines

--- main.go
+++ main.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
+	// opening the file in read only mode. The file must exist(in the current working directory)
+	// need os.Args[1][10:] later for file name
+	file, _ := os.Open("given.txt")
+	scanned := bufio.NewScanner(file)
+
+	// s
+	scanned.Split(bufio.ScanLines)
+
+	var lines []string
+
+	// append each line into a slice of strings
+	for scanned.Scan() {
+		lines = append(lines, scanned.Text())
+	}
+	file.Close()
+
+	// figure a way to print the chars vertically
+
+	for j := 0; j < 81; j++ {
+		for i := 0; i < len(lines); i++ {
+			if lines[i][j] != 32 {
+				fmt.Print(string(lines[i][j]))
+				// fmt.Print("\n")
+			}
+		}
+		fmt.Println()
+	}
+
+	// a := strings.Split(lines, "$")
+	// fmt.Println(a)
+	// fmt.Println((lines[0][0]))
 }
