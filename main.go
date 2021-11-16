953a3230c30fcf854609672c8890ee16e3f68df7
eternal17 <sarmadkhatri15@gmail.com>
2021-11-16 20:03:41 +0000

    reading both given ascii and all chars

--- main.go
+++ main.go
@@ -7,12 +7,11 @@ import (
 )
 
 func main() {
-	// opening the file in read only mode. The file must exist(in the current working directory)
+	// opening the given ascii file in read only mode. The file must exist(in the current working directory)
 	// need os.Args[1][10:] later for file name
-	file, _ := os.Open("given.txt")
-	scanned := bufio.NewScanner(file)
+	givenfile, _ := os.Open("given.txt")
+	scanned := bufio.NewScanner(givenfile)
 
-	// s
 	scanned.Split(bufio.ScanLines)
 
 	var lines []string
@@ -21,21 +20,76 @@ func main() {
 	for scanned.Scan() {
 		lines = append(lines, scanned.Text())
 	}
-	file.Close()
+	givenfile.Close()
+
+	// opening the standard txt file with all the chars, appending all lines to a slice of string
+	asciifile, _ := os.Open("standard.txt")
+	scanLines := bufio.NewScanner(asciifile)
+
+	scanLines.Split(bufio.ScanLines)
+
+	var asciiLines []string
+
+	for scanLines.Scan() {
+		asciiLines = append(asciiLines, scanLines.Text())
+	}
+
+	asciifile.Close()
+
+	// create a map and append all ascii chars, line number corresponding to ascii number
+
+	asciiMap := make(map[int][]string)
+
+	asciiNum := 31
+
+	for _, line := range asciiLines {
+		if line == "" {
+			asciiNum++
+		} else {
+			asciiMap[asciiNum] = append(asciiMap[asciiNum], line)
+		}
+	}
+
+	// for _, line := range lines {
+	// 	for _, ch := range line {
+	// 		fmt.Println(rune(ch))
+	// 	}
+	// }
 
 	// figure a way to print the chars vertically
 
 	for j := 0; j < 81; j++ {
 		for i := 0; i < len(lines); i++ {
 			if lines[i][j] != 32 {
-				fmt.Print(string(lines[i][j]))
+				fmt.Print(rune(lines[i][j]))
 				// fmt.Print("\n")
 			}
 		}
 		fmt.Println()
 	}
 
-	// a := strings.Split(lines, "$")
-	// fmt.Println(a)
-	// fmt.Println((lines[0][0]))
+	// for _, line := range asciiMap[72] {
+	// 	for _, ch := range line {
+	// 		if ch != 32 {
+	// 			fmt.Print(rune(ch))
+	// 			fmt.Print("\n")
+	// 		}
+	// 	}
+	// }
+
+	// for i := 0; i < 8; i++ {
+	// 	for _, ch := range asciiMap[72][i] {
+	// 		if ch != 32 {
+	// 			fmt.Println(rune(ch))
+	// 		}
+	// 	}
+	// }
+
+	// for i := 0; i < 8; i++ {
+	// 	for j, line := range asciiLines {
+	// 		if line != "" && asciiLines[j][i] != 32 {
+	// 			fmt.Print(asciiLines[j][i])
+	// 		}
+	// 	}
+	// }
 }
